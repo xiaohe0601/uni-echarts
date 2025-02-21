8cfc19b3ab28666d346282c828464b41537eef7f
xiaohe0601 <xiaohe0601@outlook.com>
2025-02-21 10:33:46 +0800

    feat: 新增手动更新示例

--- playground/src/pages/examples/manual-update/index.vue
+++ playground/src/pages/examples/manual-update/index.vue
@@ -1,11 +1,50 @@
 <template>
   <app-page>
-
+    <uni-echarts
+      ref="chartEl"
+      class="h-75"
+      manual-update
+      autoresize></uni-echarts>
   </app-page>
 </template>
 
 <script lang="ts" setup>
+import { LineChart } from "echarts/charts";
+import {
+  DataZoomComponent,
+  GridComponent,
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent
+} from "echarts/components";
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { getData } from "./data.ts";
+import type { UniEchartsInst } from "@/uni_modules/xiaohe-echarts";
 
+use([
+  TooltipComponent,
+  TitleComponent,
+  ToolboxComponent,
+  GridComponent,
+  DataZoomComponent,
+  LineChart,
+  CanvasRenderer
+]);
+
+const chartEl = ref<UniEchartsInst | null>(null);
+
+function update() {
+  if (chartEl.value == null) {
+    return;
+  }
+
+  chartEl.value.setOption(getData());
+}
+
+onReady(() => {
+  update();
+});
 </script>
 
 <route>
