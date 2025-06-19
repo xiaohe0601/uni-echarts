53c073aa55d27c4b800f8336465798f88f99687d
xiaohe0601 <xiaohe0601@outlook.com>
2025-06-19 20:19:13 +0800

    chore: eslint fix

--- packages/core/src/composables/usePublicApi.d.ts
+++ packages/core/src/composables/usePublicApi.d.ts
@@ -4,20 +4,20 @@ import type { NullableValue } from "../types";
 
 export type PublicApi = Pick<
   EChartsType,
-  "getWidth" |
-  "getHeight" |
-  "getDom" |
-  "getOption" |
-  "dispatchAction" |
-  "convertToPixel" |
-  "convertFromPixel" |
-  "containPixel" |
-  "getDataURL" |
-  "getConnectedDataURL" |
-  "appendData" |
-  "clear" |
-  "isDisposed" |
-  "dispose"
+  "getWidth"
+  | "getHeight"
+  | "getDom"
+  | "getOption"
+  | "dispatchAction"
+  | "convertToPixel"
+  | "convertFromPixel"
+  | "containPixel"
+  | "getDataURL"
+  | "getConnectedDataURL"
+  | "appendData"
+  | "clear"
+  | "isDisposed"
+  | "dispose"
 >;
 
 export declare function usePublicApi(chart: Ref<NullableValue<EChartsType>>): PublicApi;
