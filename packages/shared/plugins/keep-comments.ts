43eedd09b4ebbf27929752e741a965334a8d5ed2
Ethan Yin <33643257+OFreshman@users.noreply.github.com>
2025-07-16 15:24:00 +0800

    build: implement keep-comments plugin (#8)
    Co-authored-by: xiaohe0601 <xiaohe0601@outlook.com>

--- packages/shared/plugins/keep-comments.ts
+++ packages/shared/plugins/keep-comments.ts
@@ -1,11 +1,55 @@
 import type { Plugin } from "rollup";
 
 export interface KeepCommentsOptions {
-  include?: string[];
+  comments?: string[];
 }
 
+const KEEP_KEY = "!__KP__";
+
 export function keepComments(options: KeepCommentsOptions = {}): Plugin {
+  const {
+    comments = []
+  } = options;
+
   return {
-    name: "keep-comments"
+    name: "keep-comments",
+    transform: {
+      order: "pre",
+      handler(code) {
+        return {
+          code: code.replace(
+            /(\/\/[^\n]*|\/\*[\s\S]*?\*\/)/g,
+            (chunk) => {
+              for (const item of comments) {
+                if (!chunk.startsWith(item)) {
+                  continue;
+                }
+
+                // // xxx -> //!__KP__ xxx
+                // /* xxx -> /*!__KP__ xxx
+                return `${chunk.slice(0, 2)}${KEEP_KEY}${chunk.slice(2)}`;
+              }
+
+              return chunk;
+            }
+          )
+        };
+      }
+    },
+    generateBundle(_, bundle) {
+      for (const chunk of Object.values(bundle)) {
+        if (chunk.type !== "chunk") {
+          continue;
+        }
+
+        if (!chunk.code.includes(KEEP_KEY)) {
+          continue;
+        }
+
+        chunk.code = chunk.code
+          .replace(new RegExp(`/\\*${KEEP_KEY}(.*?)\\*/`, "gs"), "/*$1*/")
+          .replace(new RegExp(`//${KEEP_KEY}(.*)`, "g"), "//$1");
+      }
+    }
   };
 }
