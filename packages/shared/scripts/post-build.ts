83ec903668f8d96937fd846a463f296b48f0c27b
xiaohe0601 <xiaohe0601@outlook.com>
2025-07-16 21:23:59 +0800

    feat: shared post-build script

---
+++ packages/shared/scripts/post-build.ts
@@ -0,0 +1,28 @@
+import { resolve } from "node:path";
+import { cwd } from "node:process";
+import chalk from "chalk";
+import { consola } from "consola";
+import { copy, remove } from "fs-extra";
+
+export function r(...paths: string[]) {
+  return resolve(cwd(), ".", ...paths);
+}
+
+async function main() {
+  try {
+    consola.info(chalk.cyan("Migrating shared"));
+
+    await Promise.all([
+      copy(r("dist", "index.mjs"), r("..", "core", "src", "shared.js")),
+      copy(r("dist", "index.d.ts"), r("..", "core", "src", "shared.d.ts"))
+    ]);
+
+    await remove(r("dist"));
+
+    consola.success(chalk.green("Migrate succeeded for shared"));
+  } catch (error) {
+    consola.error("Migrate failed for `shared`", error);
+  }
+}
+
+void main();
