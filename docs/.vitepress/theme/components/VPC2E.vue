e0878255904d5f0f91bee11c4b2e89a7f663a609
xiaohe0601 <xiaohe0601@outlook.com>
2025-10-08 17:37:03 +0800

    wip: once more

--- docs/.vitepress/theme/components/VPC2E.vue
+++ docs/.vitepress/theme/components/VPC2E.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="VPC2E">
-    <button @click="test()">测试</button>
+    <button @click="transform()">转换</button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { fileOpen } from "browser-fs-access";
 
-async function test() {
-  const blob = await fileOpen({
+async function transform() {
+  const file = await fileOpen({
     extensions: [".js"]
   });
 
   const data = new FormData();
-  data.append("file", blob);
+  data.set("file", file);
 
   const res = await fetch("http://localhost:9863/api/c2e", {
     method: "POST",
     body: data
   });
 
-  console.log(res);
+  if (!res.ok) {
+    return;
+  }
+
+  const blob = await res.blob();
+
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "echarts.esm.js";
+  a.click();
+  URL.revokeObjectURL(url);
 }
 </script>
 
--- packages/server/src/api/c2e.post.ts
+++ packages/server/src/api/c2e.post.ts
@@ -1,9 +1,27 @@
+import { Readable } from "node:stream";
+import { c2e } from "@uni-echarts/c2e";
+
 export default eventHandler(async (event) => {
   const data = await readFormData(event);
 
   const file = data.get("file");
 
-  console.log(file);
+  if (!(file instanceof File) || file.type !== "text/javascript") {
+    throw new TypeError("Invalid file");
+  }
 
-  return "Uni ECharts c2e!";
+  const code = await file.text();
+
+  const { output: [chunk] } = await c2e({
+    write: false,
+    code
+  });
+
+  event.node.res.setHeader("Content-Type", "text/javascript");
+  event.node.res.setHeader(
+    "Content-Disposition",
+    `attachment; filename="${encodeURIComponent("echarts.esm.js")}"`
+  );
+
+  return Readable.from(chunk.code);
 });
