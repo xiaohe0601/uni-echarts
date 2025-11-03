b5444592631924ee54899e4e58f82dfebdbb8d6b
xiaohe0601 <xiaohe0601@outlook.com>
2025-11-03 11:32:58 +0800

    chore: update pncat config

--- pncat.config.ts
+++ pncat.config.ts
@@ -3,76 +3,30 @@ import { defineConfig, mergeCatalogRules } from "pncat";
 export default defineConfig({
   catalogRules: mergeCatalogRules([
     {
-      name: "build",
-      match: [
-        /^@rollup/,
-        /^@uni-helper\/plugin-uni$/
-      ],
-      priority: 1
-    },
-    {
-      name: "cli",
-      match: [
-        /^@antfu\/ni$/
-      ],
-      priority: 1
-    },
-    {
       name: "dcloudio",
       match: [
         /^@dcloudio/
       ],
-      priority: 0
+      priority: 1
     },
     {
       name: "dev",
       match: [
-        /^@vue\/runtime-core$/
+        "@vue/runtime-core"
       ],
-      priority: 0
+      priority: 1
     },
     {
       name: "docs",
       match: [
         /\bvitepress\b/
       ],
-      priority: 0
-    },
-    {
-      name: "node",
-      match: [
-        /^minimist$/
-      ],
       priority: 1
     },
     {
       name: "server",
       match: [
-        /^nitropack$/
-      ],
-      priority: 1
-    },
-    {
-      name: "test",
-      match: [
-        /\bvitest\b/
-      ],
-      priority: 1
-    },
-    {
-      name: "types",
-      match: [
-        /\btypes\b/,
-        /\btypings\b/
-      ],
-      priority: 1
-    },
-    {
-      name: "utils",
-      match: [
-        /^browser-fs-access$/,
-        /^es-toolkit$/,
-        /\boxc\b/
+        "nitropack"
       ],
       priority: 1
     },
@@ -82,6 +36,44 @@ export default defineConfig({
         /^@xiaohe01/
       ],
       priority: 1
+    },
+    {
+      name: "types",
+      match: [
+        /\btypes\b/,
+        /\btypings\b/
+      ],
+      priority: 10
+    },
+    {
+      name: "cli",
+      match: [
+        "@antfu/ni"
+      ],
+      priority: 20
+    },
+    {
+      name: "build",
+      match: [
+        "@uni-helper/plugin-uni"
+      ],
+      priority: 40
+    },
+    {
+      name: "node",
+      match: [
+        "minimist"
+      ],
+      priority: 60
+    },
+    {
+      name: "utils",
+      match: [
+        "browser-fs-access",
+        "es-toolkit",
+        /\boxc\b/
+      ],
+      priority: 60
     }
   ])
 });
