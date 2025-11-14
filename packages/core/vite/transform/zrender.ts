44d6e787ba2a7f1476ad489ad425fa6795f56321
xiaohe0601 <xiaohe0601@outlook.com>
2025-11-14 13:53:14 +0800

    feat(vite): PLATFORM_API

--- packages/core/vite/transform/zrender.ts
+++ packages/core/vite/transform/zrender.ts
@@ -1,7 +1,6 @@
 import { MagicString } from "vue/compiler-sfc";
+import { PLATFORM_API, SUPPORTED_ECHARTS_ENV } from "../constants";
 import type { ResolvedOptions } from "../options";
-
-const ECHARTS_SUPPORTED_ENV = new Set(["web", "h5", "mp-weixin"]);
 
 export async function transformZRender(code: string, options: ResolvedOptions) {
   if (!options.echarts.polyfill) {
@@ -9,24 +8,30 @@ export async function transformZRender(code: string, options: ResolvedOptions) {
   }
 
   // eslint-disable-next-line node/prefer-global/process
-  if (ECHARTS_SUPPORTED_ENV.has(process.env.UNI_PLATFORM!)) {
+  const platform = process.env.UNI_PLATFORM!;
+
+  if (SUPPORTED_ECHARTS_ENV.has(platform)) {
     return null;
   }
 
   const ms = new MagicString(code);
 
   if (options.echarts.polyfill) {
-    polyfillZRender(code, ms);
+    polyfillZRender(code, ms, {
+      platform
+    });
   }
 
   return ms;
 }
 
-function polyfillZRender(code: string, ms: MagicString) {
+function polyfillZRender(code: string, ms: MagicString, options: {
+  platform: string;
+}) {
   for (const item of code.matchAll(/\bwx\b/g)) {
     const start = item.index;
     const end = start + item[0].length;
 
-    ms.update(start, end, "uni");
+    ms.update(start, end, PLATFORM_API.get(options.platform) || "uni");
   }
 }
