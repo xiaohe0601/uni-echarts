fd932b897ea66e5c525a1c1f453cde78db5b4db4
xiaohe0601 <xiaohe0601@outlook.com>
2025-12-08 13:15:51 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,23 +4,12 @@ export default defineConfig({
   pnpm: true,
   ignores: [
     "**/*.min.js",
-    "packages/core/src/shared-core.*",
     "packages/core/dist-resolver",
     "packages/core/dist-vite",
+    "packages/core/src/shared-core.*",
     "packages/server/.nitro",
     "playground/src/uni_modules"
   ]
-}, {
-  files: ["pnpm-workspace.yaml"],
-  rules: {
-    "pnpm/yaml-enforce-settings": ["error", {
-      settings: {
-        catalogMode: "prefer",
-        cleanupUnusedCatalogs: true,
-        shellEmulator: true
-      }
-    }]
-  }
 }, {
   files: ["docs/**/*.vue"],
   rules: {
