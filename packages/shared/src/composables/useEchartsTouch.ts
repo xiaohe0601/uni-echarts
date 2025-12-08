41420c0c4c80c4075ec59b8e1a881b365080359a
kevin <1083464029@qq.com>
2025-12-08 12:11:31 +0800

    fix: 修复 legacy canvas 滚动后触摸交互失效问题 (#45)
    Co-authored-by: yuanchengzhang <yuancheng.zhang@lumin-ai.cn>
    Co-authored-by: xiaohe0601 <xiaohe0601@outlook.com>

--- packages/shared/src/composables/useEchartsTouch.ts
+++ packages/shared/src/composables/useEchartsTouch.ts
@@ -107,6 +107,8 @@ export function useEchartsTouch({
   function onStart(event: TouchEvent) {
     touching.value = true;
 
+    state.t = Date.now();
+
     const next = () => {
       if (chart.value == null) {
         return;
@@ -116,7 +118,6 @@ export function useEchartsTouch({
 
       state.x = touch.x;
       state.y = touch.y;
-      state.t = Date.now();
 
       const { handler } = chart.value.getZr();
       UniCanvas.dispatch(handler, "mousedown", touch);
@@ -126,19 +127,14 @@ export function useEchartsTouch({
       destroyTimer();
     };
 
-    if (isPc) {
-      querySelect(vueThis, `#${canvasId}`, {
-        rect: true
-      }).then(({ top, left }) => {
-        canvasRect.top = top!;
-        canvasRect.left = left!;
-
-        next();
-      });
-      return;
-    }
-
-    next();
+    querySelect(vueThis, `#${canvasId}`, {
+      rect: true
+    }).then(({ top, left }) => {
+      canvasRect.top = top!;
+      canvasRect.left = left!;
+    }).finally(() => {
+      next();
+    });
   }
 
   function _onMove(event: TouchEvent) {
