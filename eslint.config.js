dd44adbba99566bc70ebacf4f97eb03623f35a27
xiaohe0601 <xiaohe0601@outlook.com>
2025-12-15 12:09:25 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "@xiaohe01/eslint-config";
 
 export default defineConfig({
-  pnpm: true,
   ignores: [
-    "**/*.min.js",
-    "packages/core/dist-resolver",
-    "packages/core/dist-vite",
-    "packages/core/src/shared-core.*",
-    "packages/server/.nitro",
-    "playground/src/uni_modules"
+    "./**/*.min.js",
+    "./packages/core/dist-resolver",
+    "./packages/core/dist-vite",
+    "./packages/core/src/shared-core.*",
+    "./packages/server/.nitro",
+    "./playground/src/uni_modules"
   ]
 }, {
   files: ["docs/**/*.vue"],
